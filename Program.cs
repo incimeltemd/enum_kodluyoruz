f88046a75367d6000088f6b77c59087abd32aa52
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-04-14 18:17:45 +0300

    ödev yapıldı

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace enumm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine((int)günler.Cumartesi);
+        int sıcaklık = 32;
+        if(sıcaklık<=Sıcaklık.soğuk)
+        {
+            Console.WriteLine("hava çok soğuk bekle lütfen")
+        }
+        else if(sıcaklık>=(int)Sıcaklık.sıcak)
+        {
+            Console.WriteLine("hava sıcakk kremini unutma")
+        }
+        else if(sıcaklık>(int)Sıcaklık.ılık && sıcaklık<(int)Sıcaklık.çoksıcak)
+        {
+            Console.WriteLine("dışarı çıkman için oldukça sıcak")
+        }
+        }
+    }
+    enum günler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum Sıcaklık
+    {
+        soğuk=5,
+        ılık=15,
+        sıcak= 25,
+        çoksıcak= 30
+    }
+}
